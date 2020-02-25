45fcbd1be05ee7f4a340e2e215f5375d558c0f67
dennisn <denonjugush@gmail.com>
2020-02-25 16:33:12 +0200

    added missing functions

--- test_ml.py
+++ test_ml.py
@@ -13,6 +13,26 @@ from sklearn.metrics import adjusted_rand_score
 from sklearn.metrics import f1_score
 
 
+def compute_f1(c1, c2):
+    """
+    Compute F1 scores
+    """
+    n = c1.shape[0]
+    l1 = []
+    l2 = []
+    for i in range(n):
+        for j in range(i+1, n):
+            if c1[i] == c1[j]:
+                l1.append(1)
+            else:
+                l1.append(0)
+            if c2[i] == c2[j]:
+                l2.append(1)
+            else:
+                l2.append(0)
+    return f1_score(l1, l2)
+
+
 def regression(X_train, y_train, X_test, alpha):
     """
     Specify and fit a Ridge Linear Regression model
@@ -86,3 +106,62 @@ def run_crime_prediction(emb):
     res = {"mae": mae, "rmse": rmse, "r2": r2, "msle": msle}
     with open('cp.json', 'w') as fp:
         json.dump(res, fp, indent=4)
+
+
+def run_lu_classify(emb):
+    """
+    
+    """
+    data_path = "../data/"
+    lu = np.load(data_path + "lu_counts.npy")
+
+    transformer = TfidfTransformer()
+    tfidf = transformer.fit_transform(lu)
+    lu = tfidf.toarray()
+
+    res = {}
+    for n in range(5, 21, 5):
+        res[n] = {}
+        kmeans = KMeans(n_clusters=n, random_state=3)
+        emb_labels = kmeans.fit_predict(emb)
+
+        kmeans = KMeans(n_clusters=n, random_state=3)
+        lu_labels = kmeans.fit_predict(lu)
+
+        print("=====================================")
+        nmi = normalized_mutual_info_score(lu_labels, emb_labels)
+        print("emb nmi: {}".format(nmi))
+        res[n]["nmi"] = nmi
+        print("------------------------------------")
+        ars = adjusted_rand_score(lu_labels, emb_labels)
+        print("emb ars: {}".format(ars))
+        res[n]["ars"] = ars
+        print("------------------------------------")
+        f1 = compute_f1(lu_labels, emb_labels)
+        print("emb f1: {}".format(f1))
+        res[n]["f1"] = f1
+    with open('lu.json', 'w') as fp:
+        json.dump(res, fp, indent=4)
+
+
+def reformEmbed(ZoneEmbed, Num_zone):
+    """
+    Reshape the embeddings
+    """
+    ZoneEmbed_shape = np.shape(ZoneEmbed)
+
+    emb = np.zeros((Num_zone,
+                    int(ZoneEmbed_shape[0]/Num_zone*ZoneEmbed_shape[1])))
+    for index_1 in range(ZoneEmbed_shape[0]):
+        for index_2 in range(ZoneEmbed_shape[1]):
+            emb[index_1 % Num_zone, index_2 + ZoneEmbed_shape[1]*int(index_1/Num_zone)] \
+                = ZoneEmbed[index_1, index_2]
+    return emb
+
+
+if __name__ == "__main__":
+    Num_zone = 180
+    ZoneEmbed = np.load('../data/ZoneEmbed.npy')
+    emb = reformEmbed(ZoneEmbed, Num_zone)
+    run_lu_classify(emb)
+    run_crime_prediction(emb)
